0a3f04e53e3b973fcf1a86426f88c3740673775a
NickVelos420 <velosn@gmail.com>
2021-08-02 17:31:49 +0300

    gets username form cookie and shows chatrooms

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,24 +1,52 @@
-// add a useEffect to check if the user has a profile picture and change the
-import { FC, useState } from "react";
+// add a useEffect to check if the user has a profile picture and change the src
+import { FC, useState, useEffect } from "react";
 import Image from "next/image";
+import { useCookies } from "../../hooks/useCookies";
 const userDefaultImage = require("../../images/userDefaultImage.jpg");
 const settingsGear = require("../../images/gearIcon.png");
 
-const Profile: FC = () => {
-	const [username, setUsername] = useState("");
-	const [chatRooms, setChatRooms] = useState([]);
+interface User {
+	_id: string;
+	id: string;
+	username: string;
+	email: string;
+	password: string;
+	__v: number;
+}
 
-	// TODO: make a request to the backend to get the username
+const userCookie = useCookies(false, false, false, true)?.user;
+
+const Profile: FC = props => {
+	const [username, setUsername] = useState("");
+	const [chatRooms, setChatRooms] = useState(["chat room"]);
+	const [friends, setFriends] = useState([]);
+
+	useEffect(() => {
+		if (userCookie) {
+			const userCookieJSON: User = JSON.parse(userCookie);
+
+			return setUsername(userCookieJSON.username);
+		}
+	}, []);
 
 	return (
-		<>
-			<Image src={userDefaultImage} alt="Profile" height="100" width="100" />
-			<div>Username</div>
-			<label>
+		<div>
+			<div>
+				<Image src={userDefaultImage} alt="Profile" height="100" width="100" />
+			</div>
+			<div className="username">Username</div>
+			<label htmlFor="settings">
 				<span>Settings</span>
-				<Image src={settingsGear} />
 			</label>
-		</>
+
+			<a id="settings" href="/loggedIn/profile/settings">
+				<Image src={settingsGear} height="100" width="100" alt="settings gear" />
+			</a>
+			{chatRooms.map(chatRoom => (
+				<div key="hey">{chatRoom}</div>
+			))}
+			{username}
+		</div>
 	);
 };
 
