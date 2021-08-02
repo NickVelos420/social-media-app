55e591ecd7805ec6927d8af2841eed04d175c52f
NickVelos420 <velosn@gmail.com>
2021-08-02 17:20:57 +0300

    changed setCookie's param types

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import LoginForm from "./LoginForm";
 
 interface propTypes {
-	setCookie: (key: string, value: any) => string;
+	setCookie: (key: string, value: any, expirationDate: any) => string;
 }
 
 const Login: FC<propTypes> = ({ setCookie }) => {
