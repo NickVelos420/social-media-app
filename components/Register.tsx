2e2d9449a89d6cffe30ed0ee7f4457e994f7d2ca
NickVelos420 <velosn@gmail.com>
2021-08-02 17:29:06 +0300

    added link to login

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,16 +1,26 @@
+import Link from "next/link";
 import { FC } from "react";
 import RegisterForm from "./RegisterForm";
 
 interface propTypes {
-	setCookie: (key: string, value: any) => string;
+	setCookie: (key: string, value: any, expirationDate: any) => string;
 }
 
-const Login: FC<propTypes> = ({ setCookie }) => {
+const Register: FC<propTypes> = ({ setCookie }) => {
 	return (
 		<>
 			<RegisterForm setCookie={setCookie} />
+			<div>
+				<span>Already have an account?</span>
+				<br />
+				<span>Click </span>
+				<Link href="/login">
+					<button>Here</button>
+				</Link>
+				<span> to login</span>
+			</div>
 		</>
 	);
 };
 
-export default Login;
+export default Register;
