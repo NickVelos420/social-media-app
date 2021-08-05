a3b9c4dafe47324e525b59fd37e98b778b25c398
NickVelos420 <velosn@gmail.com>
2021-08-05 20:31:57 +0300

    added password requirements

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, ChangeEvent, FC, useState } from "react";
 import axios from "axios";
+import { usePasswordRequirements } from "../hooks/usePasswordRequirements";
 
 interface propTypes {
 	setCookie: (key: string, value: any, expirationDate: any) => string;
@@ -9,9 +10,17 @@ const LoginForm: FC<propTypes> = ({ setCookie }) => {
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
 
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault();
+		const { message, booleanValue } = usePasswordRequirements(password);
+
+		if (!booleanValue) {
+			return setError(message);
+		}
+
+		setError("");
 
 		try {
 			let res = await axios.post("http://localhost:4000/register", { username, password, email });
@@ -27,14 +36,25 @@ const LoginForm: FC<propTypes> = ({ setCookie }) => {
 
 	return (
 		<>
+			<div style={{ color: "#e01919", fontSize: "1.5rem" }}>{error}</div>
+			<div>
+				<span>Password Validation Rules</span>
+				<ul>
+					<li>The Password Should Be from 7 To 300 Characters</li>
+					<li>It Must Be English</li>
+					<li>It Should Contain At Least 1 Number</li>
+					<li>It Should Contain At Least 1 Upper Case Character</li>
+					<li>
+						It Should Contain At Least 1 of the following special characters (!,@,#,$,%,^,&,*,?,|)
+					</li>
+				</ul>
+			</div>
 			<form onSubmit={handleSubmit}>
 				{/* USERNAME FORM */}
 				<label>
 					<span>Username</span>
 					<input
 						required
-						min="6"
-						max="28"
 						type="text"
 						name="username"
 						placeholder="Username"
@@ -59,8 +79,6 @@ const LoginForm: FC<propTypes> = ({ setCookie }) => {
 					<span>Password</span>
 					<input
 						required
-						min="7"
-						max="350"
 						type="password"
 						name="password"
 						placeholder="Password"
@@ -68,7 +86,7 @@ const LoginForm: FC<propTypes> = ({ setCookie }) => {
 					/>
 				</label>
 
-				<input type="submit" />
+				<input type="submit" value="Register" />
 			</form>
 		</>
 	);
