6ed498c295a1d10d8bd7d1544bc714d0c67bf07e
NickVelos420 <velosn@gmail.com>
2021-08-16 20:30:48 +0300

    added getCookies IFace  and pass it to Register

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,14 +2,32 @@ import { FC } from "react";
 import Register from "./Register";
 import styles from "../styles/forReactComponents/main.module.scss";
 
-interface propTypes {
-	setCookie: (key: string, value: any, expirationDate: any) => string;
+interface UserDataTypes {
+	_id: string;
+	id: string;
+	username: string;
+	email: string;
+	__v: number;
 }
 
-const Main: FC<propTypes> = ({ setCookie }) => {
+interface getCookiesReturnTypes {
+	user?: UserDataTypes;
+}
+
+interface propTypes {
+	setCookie: (key: string, value: any, expirationDate: any) => string;
+	getCookies: (
+		key: boolean,
+		value: any,
+		expirationDate: any,
+		getCookies: boolean
+	) => getCookiesReturnTypes;
+}
+
+const Main: FC<propTypes> = ({ setCookie, getCookies }) => {
 	return (
 		<div className={styles.body}>
-			<Register setCookie={setCookie} />
+			<Register setCookie={setCookie} getCookies={getCookies} />
 		</div>
 	);
 };
