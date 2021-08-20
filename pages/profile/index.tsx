b5f8568b9fe1f1302cc82019b381b02917ae7191
NickVelos420 <velosn@gmail.com>
2021-08-20 19:58:16 +0300

    now don't show an error if isn't logged in

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,25 +1,9 @@
 import { FC, useEffect, useState } from "react";
 import Profile from "../../components/profile/Profile";
-import useIsLoggedIn from "../../hooks/useIsLoggedIn";
-import NotLoggedInErrorScreen from "../../components/NotLoggedInErrorScreen";
 import { GetServerSideProps } from "next";
 import { redirectIfUserIsntLoggedIn } from "../../randomFunctions/redirectIfIsntLoggedIn";
 
 const index: FC = () => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-	useEffect(() => {
-		setIsLoggedIn(useIsLoggedIn());
-	}, []);
-
-	if (!isLoggedIn) {
-		return (
-			<div>
-				<NotLoggedInErrorScreen />
-			</div>
-		);
-	}
-
 	return (
 		<div>
 			<Profile />
