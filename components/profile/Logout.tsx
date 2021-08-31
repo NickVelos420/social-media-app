88488f6d443c9cd7bc90e65a8411b5e43b57c484
NickVelos420 <velosn@gmail.com>
2021-08-31 21:35:34 +0300

    new logout component

---
+++ components/profile/Logout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logout = () => {
+	return (
+		<div>
+			<p>Why would you want to log out of this beautiful app?</p>
+		</div>
+	);
+};
+
+export default Logout;
