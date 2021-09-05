bdaa49e11b0e78e6bc784cc326d3958809787fe7
NickVelos420 <velosn@gmail.com>
2021-09-05 21:04:54 +0300

    using the tabs

--- components/profile/Settings.tsx
+++ components/profile/Settings.tsx
@@ -1,17 +1,36 @@
 import { FC, useState } from "react";
+import Tab from "../tabs/Tab";
+import TabNav from "../tabs/TabNav";
 import Logout from "./Logout";
 
 const Settings: FC = () => {
-	const [showLogout, setShowLogout] = useState(false);
+	const [selectedTab, setSelectedTab] = useState("");
+
 	return (
 		<div>
-			<button
-				onClick={() => {
-					setShowLogout(!showLogout);
-				}}
+			{/* <details>
+				<summary>Log out</summary>
+				<Logout />
+			</details>
+			<style jsx>{`
+				summary:hover {
+					cursor: pointer;
+				}
+			`}</style> */}
+
+			<TabNav
+				tabs={["logout", "login", "yes"]}
+				setSelectedTab={setSelectedTab}
+				selectedTab={selectedTab}
 			>
-				Log Out
-			</button>
+				<Tab isSelected={selectedTab === "logout"}>
+					<Logout />
+				</Tab>
+				<Tab isSelected={selectedTab === "login"}>
+					<h1>Hello there m8 so nice to meet you</h1>
+				</Tab>
+				<Tab isSelected={selectedTab === "yes"}>yes it's a me mario</Tab>
+			</TabNav>
 		</div>
 	);
 };
