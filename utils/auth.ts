9e245045a595b1d626f734cf97b0c58d4e595f9f
NickVelos420 <velosn@gmail.com>
2021-09-16 20:32:03 +0300

    added passwordReqs to the auth file

--- utils/auth.ts
+++ utils/auth.ts
@@ -5,3 +5,22 @@ export const logout = (cookieName: string) => {
 	useCookies({ name: cookieName, deleteCookie: true });
 	Router.push("/login");
 };
+
+interface passwordReqReturnTypes {
+	message: string;
+	booleanValue: boolean;
+}
+
+export const addPasswordRequirements = (password: string): passwordReqReturnTypes => {
+	const passwordValidationRegex =
+		/(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*?|])[a-zA-Z0-9!@#$%^&*?|]/g;
+
+	if (password.length <= 7) {
+		return { message: "The password is too small", booleanValue: false };
+	} else if (password.length > 300) {
+		return { message: "The password is too long", booleanValue: false };
+	} else if (!passwordValidationRegex.test(password)) {
+		return { message: "password doesn't match the validation rules", booleanValue: false };
+	}
+	return { message: "none", booleanValue: true };
+};
