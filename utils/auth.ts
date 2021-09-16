1b0aae160da7b999855071c93b9385d77c6bb5cd
NickVelos420 <velosn@gmail.com>
2021-09-16 20:20:15 +0300

    using the new useCookies form

--- utils/auth.ts
+++ utils/auth.ts
@@ -2,6 +2,6 @@ import Router from "next/router";
 import { useCookies } from "../hooks/useCookies";
 
 export const logout = (cookieName: string) => {
-	useCookies(cookieName, undefined, 0, false, false, false, true);
+	useCookies({ name: cookieName, deleteCookie: true });
 	Router.push("/login");
 };
