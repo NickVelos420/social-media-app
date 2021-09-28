cc3bf8f9f7df8b43b8682204ce18425d183c2b9c
NickVelos420 <velosn@gmail.com>
2021-09-28 18:39:11 +0300

    renamed login.tsx to index and added folder

---
+++ pages/login/index.tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps } from "next";
+import { FC } from "react";
+import Layout from "../../components/Layout";
+import Login from "../../components/Login";
+import { useCookies } from "../../hooks/useCookies";
+import { redirectIfUserIsLoggedIn } from "../../utils/redirectIfUserIsLoggedIn";
+
+const login: FC = () => {
+	return (
+		<Layout title="Sign In">
+			<Login setCookie={useCookies} />
+		</Layout>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = redirectIfUserIsLoggedIn;
+
+export default login;
