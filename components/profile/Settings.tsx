8fa8509e110742047906f3376ad9edc250f99f4d
NickVelos420 <velosn@gmail.com>
2021-09-28 19:15:02 +0300

    added new settings to change password

--- components/profile/Settings.tsx
+++ components/profile/Settings.tsx
@@ -2,16 +2,22 @@ import { FC, useState } from "react";
 import Tab from "../tabs/Tab";
 import TabNav from "../tabs/TabNav";
 import Logout from "./Logout";
+import Link from "next/link";
 
 const Settings: FC = () => {
 	const [selectedTab, setSelectedTab] = useState("");
-	const tabs = ["logout"];
+	const tabs = ["logout", "change password"];
 
 	return (
 		<div>
 			<TabNav tabs={tabs} setSelectedTab={setSelectedTab} selectedTab={selectedTab}>
 				<Tab isSelected={selectedTab === "logout"}>
 					<Logout />
+				</Tab>
+				<Tab isSelected={selectedTab === "change password"}>
+					<Link href="/login/forgot_password">
+						<a>Change Password</a>
+					</Link>
 				</Tab>
 			</TabNav>
 		</div>
