3084e55a083af4fa0a6c3dd21431ee4d53a56a64
NickVelos420 <velosn@gmail.com>
2021-11-12 13:20:22 +0200

    added bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/Global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
