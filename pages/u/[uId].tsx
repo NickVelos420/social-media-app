a93872fd680a03db21506dda15dfdf028ac8323b
NickVelos420 <velosn@gmail.com>
2021-11-28 13:08:14 +0200

    now [uId] check if users are friends

--- pages/u/[uId].tsx
+++ pages/u/[uId].tsx
@@ -1,26 +1,32 @@
 import { GetServerSideProps } from "next";
-import { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import { redirectIfUserIsntLoggedIn } from "../../utils/redirectIfIsntLoggedIn";
 import { useRouter } from "next/router";
 import {
+	checkIfUsersAreFriends,
 	getSomeonesCredentialsById,
 	rejectFriendRequest,
 	sendFriendRequest,
 } from "../../utils/social";
 import { getUserObject } from "../../utils/auth";
+import axios from "axios";
+import NotFriendsProfile from "../../components/social/NotFriendsProfile";
+import AreFriendsProfile from "../../components/social/AreFriendsProfile";
 
-const uId: FC = () => {
+const uId: FC = props => {
 	const [username, setUsername] = useState("");
 	const [receiverId, setReceiverId] = useState("");
 	const [error, setError] = useState("");
 	const [successfulFriendRequest, setSuccessFriendRequest] = useState(false);
 	const [senderId, setSenderId] = useState("");
+	const [areUsersFriends, setAreUsersFriends] = useState<boolean | null>(null);
 
 	const router = useRouter();
 
 	const { uId } = router.query;
 
+	//! Sets receiverId and username state
 	useEffect(() => {
 		(async () => {
 			if (typeof uId !== "string") return null;
@@ -31,17 +37,34 @@ const uId: FC = () => {
 		})();
 	}, [uId]);
 
+	//! Check if users are friends and sets state
 	useEffect(() => {
-		(async () => {
-			const senderUserObj = await getUserObject();
+		if (senderId && receiverId) {
+			(async () => {
+				try {
+					const res = await checkIfUsersAreFriends(senderId, receiverId);
 
-			if (!senderUserObj)
-				return setError(
-					"An error occurred while sending friend request.Try again if the error persists there might be a problem with the cookies please try logging in again and then send the friend request"
-				);
+					if (res === null) return setError("An error occurred, try again later");
 
-			setSenderId(senderUserObj.id);
-		})();
+					return res === false ? setAreUsersFriends(false) : setAreUsersFriends(true);
+				} catch (error) {
+					console.log(error);
+					setError("An error occurred, try again later");
+				}
+			})();
+		} else return;
+	}, [senderId, receiverId]);
+
+	//! Sets senderId state
+	useEffect(() => {
+		// @ts-ignore
+		if (!props?.user)
+			return setError(
+				"An error occurred while sending friend request.Try again if the error persists there might be a problem with the cookies please try logging in again and then send the friend request"
+			);
+
+		// @ts-ignore
+		setSenderId(props?.user?.id);
 	}, []);
 
 	const sendFriendRequestToUser = async () => {
@@ -77,19 +100,30 @@ const uId: FC = () => {
 		}
 	};
 
-	return (
-		<Layout title={`${username} Profile`}>
-			{error}
-			<br />
-			{username}
-			<br />
-			{!successfulFriendRequest ? (
-				<button onClick={sendFriendRequestToUser}>Add Friend</button>
-			) : (
-				<button onClick={declineFriendRequest}>Decline Sending Friend Request</button>
-			)}
-		</Layout>
-	);
+	if (senderId && receiverId) {
+		if (areUsersFriends === true) {
+			return (
+				<Layout title={`${username} Profile`}>
+					<AreFriendsProfile username={username} />
+				</Layout>
+			);
+		} else if (areUsersFriends === false) {
+			return (
+				<Layout title={`${username} Profile`}>
+					<NotFriendsProfile
+						username={username}
+						receiverId={receiverId}
+						senderId={senderId}
+						sendFriendRequest={sendFriendRequestToUser}
+						declineFriendRequest={declineFriendRequest}
+						successfulFriendRequest={successfulFriendRequest}
+					/>
+				</Layout>
+			);
+		}
+	}
+
+	return null;
 };
 
 export const getServerSideProps: GetServerSideProps = redirectIfUserIsntLoggedIn;
