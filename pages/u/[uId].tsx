78d2bf4a5ae0cc3b7a2a3a74de4b3ccaf395850f
NickVelos420 <velosn@gmail.com>
2021-12-13 19:01:54 +0200

    now check if the user has sent FR

--- pages/u/[uId].tsx
+++ pages/u/[uId].tsx
@@ -4,23 +4,28 @@ import Layout from "../../components/Layout";
 import { redirectIfUserIsntLoggedIn } from "../../utils/redirectIfIsntLoggedIn";
 import { useRouter } from "next/router";
 import {
-	checkIfUsersAreFriends,
+	acceptFriendRequest,
+	checkIfUsersAreFriendsAndIfHasSentFR,
 	getSomeonesCredentialsById,
 	rejectFriendRequest,
 	sendFriendRequest,
 } from "../../utils/social";
-import { getUserObject } from "../../utils/auth";
-import axios from "axios";
 import NotFriendsProfile from "../../components/social/NotFriendsProfile";
 import AreFriendsProfile from "../../components/social/AreFriendsProfile";
+import WhoHasSentFRProfile from "../../components/social/WhoHasSentFRProfile";
 
 const uId: FC = props => {
+	type whoSentRequestType = -1 | 0 | 1;
+
 	const [username, setUsername] = useState("");
 	const [receiverId, setReceiverId] = useState("");
 	const [error, setError] = useState("");
 	const [successfulFriendRequest, setSuccessFriendRequest] = useState(false);
 	const [senderId, setSenderId] = useState("");
 	const [areUsersFriends, setAreUsersFriends] = useState<boolean | null>(null);
+	const [hasSentFriendRequest, setHasSentFriendRequest] = useState<boolean | null>(null);
+	const [whoSentRequest, setWhoSentRequest] = useState<whoSentRequestType>(-1);
+	const [successfullyAcceptedFR, setSuccessfullyAcceptedFR] = useState<boolean | null>(null);
 
 	const router = useRouter();
 
@@ -42,11 +47,36 @@ const uId: FC = props => {
 		if (senderId && receiverId) {
 			(async () => {
 				try {
-					const res = await checkIfUsersAreFriends(senderId, receiverId);
-
+					const res = await checkIfUsersAreFriendsAndIfHasSentFR(senderId, receiverId);
 					if (res === null) return setError("An error occurred, try again later");
-
-					return res === false ? setAreUsersFriends(false) : setAreUsersFriends(true);
+					console.log(res);
+					if (
+						res.hasSent_FR_Obj?.hasSentRequest === true &&
+						res.hasSent_FR_Obj?.whoSentRequest === 0
+					) {
+						setHasSentFriendRequest(true);
+						setWhoSentRequest(0);
+						setAreUsersFriends(false);
+					} else if (
+						res.hasSent_FR_Obj?.hasSentRequest === true &&
+						res.hasSent_FR_Obj.whoSentRequest === 1
+					) {
+						setHasSentFriendRequest(true);
+						setWhoSentRequest(1);
+						setAreUsersFriends(false);
+					} else if (
+						res.hasSent_FR_Obj?.hasSentRequest === false &&
+						res.hasSent_FR_Obj?.whoSentRequest === -1
+					) {
+						setAreUsersFriends(false);
+						setHasSentFriendRequest(false);
+					} else if (res.areFriends) {
+						setAreUsersFriends(true);
+						setHasSentFriendRequest(false);
+					} else if (!res.areFriends) {
+						setAreUsersFriends(false);
+						setHasSentFriendRequest(false);
+					}
 				} catch (error) {
 					console.log(error);
 					setError("An error occurred, try again later");
@@ -82,13 +112,6 @@ const uId: FC = props => {
 
 	const declineFriendRequest = async () => {
 		try {
-			const senderUserObj = await getUserObject();
-
-			if (!senderUserObj)
-				return setError(
-					"An error occurred while sending friend request.Try again if the error persists there might be a problem with the cookies please try logging in again and then send the friend request"
-				);
-
 			const res = await rejectFriendRequest(senderId, receiverId);
 
 			if (res === "OK") {
@@ -100,11 +123,43 @@ const uId: FC = props => {
 		}
 	};
 
+	const acceptFR = async () => {
+		try {
+			const res = await acceptFriendRequest(senderId, receiverId);
+
+			if (res === "Created") {
+				setSuccessfullyAcceptedFR(true);
+				setError("");
+			}
+		} catch (error) {
+			//! why not check if they are friends
+			console.error(error);
+			setSuccessfullyAcceptedFR(false);
+			setError(
+				"An error occurred while accepting friend request.Try again if the error persists there might be a problem with the cookies please try logging in again and then accept the friend request"
+			);
+		}
+	};
+
+	console.log(areUsersFriends, hasSentFriendRequest, whoSentRequest);
+
 	if (senderId && receiverId) {
 		if (areUsersFriends === true) {
 			return (
 				<Layout title={`${username} Profile`}>
 					<AreFriendsProfile username={username} />
+				</Layout>
+			);
+		} else if (hasSentFriendRequest === true) {
+			return (
+				<Layout title={`${username}'s Profile'`}>
+					<WhoHasSentFRProfile
+						username={username}
+						declineFR={declineFriendRequest}
+						whoSentRequest={whoSentRequest}
+						acceptFR={acceptFR}
+						successfullyAcceptedFR={successfullyAcceptedFR}
+					/>
 				</Layout>
 			);
 		} else if (areUsersFriends === false) {
@@ -122,8 +177,7 @@ const uId: FC = props => {
 			);
 		}
 	}
-
-	return null;
+	return <>{error}</>;
 };
 
 export const getServerSideProps: GetServerSideProps = redirectIfUserIsntLoggedIn;
